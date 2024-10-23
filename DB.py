81b7efbee9be36c7c8794c1162dfe739aff6fc2a
An Jaebeom <ajb8533296@gmail.com>
2024-10-24 00:24:55 +0900

    aaaaa

--- DB.py
+++ DB.py
@@ -12,30 +12,3 @@ DATABASE_CONFIG = {
 
 async def get_db_connection():
     return await aiomysql.connect(**DATABASE_CONFIG)
-
-async def setup_database():
-    try:
-        conn = await get_db_connection()
-        try:
-            async with conn.cursor(aiomysql.DictCursor) as cursor:
-                await cursor.execute("""
-                    USE ramen;
-                    CREATE TABLE IF NOT EXISTS ranking (
-                        name TEXT NOT NULL,
-                        score INT NOT NULL,
-                        phone VARCHAR(15) NOT NULL,
-                        created_at DATETIME NOT NULL,
-                        PRIMARY KEY (phone)
-                    );
-                """)
-        except aiomysql.Warning as warn:
-            # 테이블이 이미 존재할 경우 경고 무시
-            pass
-        except Exception as e:
-            print("DB 쿼리 에러:", e)
-            exit()
-        finally:
-            await conn.close()
-    except Exception as e:
-        print("DB 연결 실패:", e)
-        exit()
